14abb123d306e85e7ca7c825f8f47ea371522682
Jon Bradley <jon@MacBookPro.lan>
2026-05-03 19:44:47 +0100

    Split set viewer into dynamic no-SSR component to fix server crash

--- frontend/src/app/(app)/services/[id]/set/view/page.tsx
+++ frontend/src/app/(app)/services/[id]/set/view/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 import dynamic from 'next/dynamic'
 
-const SetViewerPage = dynamic(() => import('./SetViewer').then(m => m.SetViewerPage), { ssr: false })
+const SetViewerPage = dynamic(
+  () => import('./SetViewer').then(m => m.SetViewerPage),
+  { ssr: false, loading: () => <div style={{ minHeight: '100vh', background: '#fff', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#333' }}>Loading set…</div> }
+)
 
 export default function Page() {
   return <SetViewerPage />
