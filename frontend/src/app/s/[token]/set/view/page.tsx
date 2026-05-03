6113d18aad46d8ca7cefd0459426c51e235bed78
Jon Bradley <jon@MacBookPro.lan>
2026-05-03 19:28:34 +0100

    Guard pdfjs worker setup from SSR

--- frontend/src/app/s/[token]/set/view/page.tsx
+++ frontend/src/app/s/[token]/set/view/page.tsx
@@ -7,7 +7,9 @@ import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
+if (typeof window !== 'undefined') {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
+}
 
 interface SetFile {
   url: string
