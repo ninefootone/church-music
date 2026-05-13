56f0522473966770f3e34b7dc1d844e5b5c2b224
Jon Bradley <jon@ninefootone.co.uk>
2026-05-13 14:11:07 +0100

    Fix 404 icon size and centring

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="not-found-page">
       <div className="not-found-inner">
-        <Image src="/logo-icon.svg" alt="Song Stack" width={72} height={72} className="not-found-icon" />
+        <Image src="/logo-icon.svg" alt="Song Stack" width={40} height={40} className="not-found-icon" />
         <h1 className="not-found-title">Page not found</h1>
         <p className="not-found-body">
           This page doesn&apos;t exist or may have been moved.
