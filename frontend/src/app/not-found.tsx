6f67e9936898f9086a3cab1c38e8eba4837d7ca8
Jon Bradley <jon@ninefootone.co.uk>
2026-05-13 14:07:43 +0100

    Use brand icon on 404 page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function NotFound() {
   return (
     <div className="not-found-page">
       <div className="not-found-inner">
-        <div className="not-found-icon">♪</div>
+        <Image src="/logo-icon.svg" alt="Song Stack" width={72} height={72} className="not-found-icon" />
         <h1 className="not-found-title">Page not found</h1>
         <p className="not-found-body">
           This page doesn&apos;t exist or may have been moved.
